45a25808c59c26f532c9208cdd80ccaec132460f
Damian Bushong <katana@odios.us>
2015-10-08 10:20:07 -0500

    simplify openterm.sh

--- terminator/.bin/openterm.sh
+++ terminator/.bin/openterm.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-newtermgeom="echo $(slop -c 0.918,0.525,0 -b 3 -f "%wx%h+%x+%y")"
-terminator --geometry=$(eval $newtermgeom)
+terminator --geometry=$(slop -c 0.918,0.525,0 -b 3 -f "%wx%h+%x+%y")
